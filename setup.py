5c70a45367de99e96e88d5af805a7224dc6f730a
DenisSavenkov <denis.savenkov@emory.edu>
2013-12-30 14:32:40 -0500

    Commiting missing setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='web_search_api',
+      version='0.1',
+      description='Python wrapper for Bing and Google search API',
+      url='http://github.com/DenXX/web_search_api',
+      author='Denis Savenkov',
+      author_email='denissavenkov@gmail.com',
+      license='MIT',
+      packages=['web_search_api'],
+      zip_safe=False)
